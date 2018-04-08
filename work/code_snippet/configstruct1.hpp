ae38fd38cc2c738238bb1f63905a152fedbc333a
demon90s <demon90s@163.com>
2018-04-08 20:08:29 +0800

    commit

--- work/code_snippet/configstruct1.hpp
+++ work/code_snippet/configstruct1.hpp
@@ -19,3 +19,27 @@
 	
 	CfgItem cfg_item_list[XXX_MAX_COUNT];
 };
+
+/////////////////////////////////////////////////////////////////
+
+struct XXXConfig
+{
+	struct CfgItem
+	{
+		int ReadConfig(rapidxml::xml_node<> *data_element, int _seq)
+		{
+			if (has_read) return -1;
+			has_read = 1;
+
+			seq = _seq;
+
+			return 0;
+		}
+
+		int has_read = 0;
+		int seq = 0;					// 索引
+		
+	};
+	
+	CfgItem cfg_item_list[XXX_MAX_COUNT];
+};
--- work/code_snippet/initcfg1.hpp
+++ work/code_snippet/initcfg1.hpp
@@ -1,3 +1,14 @@
+bool XxXConfig::Init(const std::string &configname, std::string *err)
+{
+	PRE_LOAD_CONFIG2;
+
+	LOAD_CONFIG2("other", InitOtherCfg);
+
+	return true;
+}
+
+/////////////////////////////////////////////////////////////////////////
+
 auto data_element = root_element->FirstChildElement("data");
 while (data_element)
 {
@@ -11,6 +22,25 @@ while (data_element)
 	}
 
 	data_element = data_element->NextSiblingElement();
+}
+
+return 0;
+
+////////////////////////////////////////////////////////////////////////
+
+auto data_element = root_element->first_node("data");
+while (data_element)
+{
+	int seq = 0;
+	if (!GetSubNodeValue(data_element, "seq", seq) || !this->IsValidXXXSeq(seq, true)) return -1000;
+
+	int ret = m_xxx_cfg.cfg_item_list[seq].ReadConfig(data_element, seq);
+	if (ret != 0)
+	{
+		return ret;
+	}
+
+	data_element = data_element->next_sibling();
 }
 
 return 0;
